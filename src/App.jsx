2402fe4530c037ed5ac1b300f63afb777ae3a9b6
Justin Hawkins <jscott3198@gmail.com>
2025-12-11 11:16:21 -0600

    added the TabButton component to the components folder and used it to create new tabs in Apps.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { CORE_CONCEPTS } from "./data.js";
 import { Header } from "./components/Header/Header.jsx";
 import { CoreConcept } from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
 function App() {
   return (
     <div>
@@ -22,6 +23,15 @@ function App() {
               image={CORE_CONCEPTS[3].image}
             />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
